34ecc72c22ea9cdc3aff8368937ff84e5801637a
nicknpnp <126056970+nicknpnp@users.noreply.github.com>
2025-05-29 01:06:32 +0800

    update contact tmp

--- tmp-about_contact.php
+++ tmp-about_contact.php
@@ -16,38 +16,63 @@
 get_header();
 ?>
 
-<?php
+<?php get_template_part('template-parts/roll-menu'); ?>
 
-while ( have_posts() ) :
-    the_post();
-?>
+<div class="section section_contact scrollin_p">
+	<div class="contact_banner">
+		<div class="section_center_content">
+			<?php
+			$contact_banner = get_field("contact_banner");
+			if ($contact_banner): ?>
+				<div class="photo"><img src="<?php echo esc_url($contact_banner['sizes']['l']); ?>"></div>
+			<?php endif; ?>
+		</div>
+	</div>
 
-    <?php $contact_banner = get_field("contact_banner");
-        echo $contact_banner ? '<div>'.esc_url($contact_banner['sizes']['xl']).'</div>' : '';
-    ?>
-    <?php if(get_field("contact_title")): ?>
-        <div><?php the_field("contact_title"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("contact_description")): ?>
-        <div><?php the_field("contact_description"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("map_latitude")): ?>
-        <div><?php the_field("map_latitude"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("map_longitude")): ?>
-        <div><?php the_field("map_longitude"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("map_zoom")): ?>
-        <div><?php the_field("map_zoom"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("free_text")): ?>
-        <div><?php the_field("free_text"); ?></div>
-    <?php endif; ?>
-    
-<?php
-endwhile; // End of the loop.
-?>
+	<div class="contact_intro">
+		<div class="section_center_content">
+			<?php if (get_field("contact_title")): ?>
+				<h1 class="section_title text1 scrollin scrollinbottom"><?php the_field("contact_title"); ?></h1>
+			<?php endif; ?>
+			<?php if (get_field("contact_description")): ?>
+				<div class="section_description scrollin scrollinbottom col6"><?php the_field("contact_description"); ?></div>
+			<?php endif; ?>
+		</div>
+	</div>
 
+	<div class="contact_bottom">
+		<div class="section_center_content">
+			<div class="col_wrapper">
+				<div class="flex row">
+					<div class="col col7">
+						<div class="col_spacing">
+							<div class="contact_map_wrapper">
+								<div class="map"
+									data-lng="<?php echo get_field('map_longitude'); ?>"
+									data-lat="<?php echo get_field('map_latitude'); ?>"
+									data-zoom="<?php echo get_field('map_zoom'); ?>">
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col col5">
+						<div class="col_spacing">
+							<div class="flexible_layout_wrapper">
+								<?php if (get_field("free_text")): ?>
+									<div class="flexible_layout flexible_layout_freetext scrollin scrollinbottom">
+										<div class="free_text">
+											<?php the_field("free_text"); ?>
+										</div>
+									</div>
+								<?php endif; ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php
 get_footer();
