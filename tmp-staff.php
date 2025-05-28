140c796b103a34e7bc5675a0c5f31c63881eccba
nicknpnp <126056970+nicknpnp@users.noreply.github.com>
2025-05-29 01:21:47 +0800

    add staff tmp

---
+++ tmp-staff.php
@@ -0,0 +1,87 @@
+<?php /* Template Name: Staff */ ?>
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package cuhk_chi
+ */
+
+get_header();
+?>
+
+<?php get_template_part('template-parts/roll-menu'); ?>
+
+<div class="section section_content">
+	<div class="section_center_content small_section_center_content">
+		<?php if (get_field("page_title")): ?>
+			<h1 class="section_title text1 scrollin scrollinbottom"><?php the_field("page_title"); ?></h1>
+		<?php endif; ?>
+		<?php if (get_field("page_description")): ?>
+			<div class="section_description scrollin scrollinbottom col6"><?php the_field("page_description"); ?></div>
+		<?php endif; ?>
+
+		<div class="section_list">
+			<?php
+			$args = array(
+				'post_type' => 'profile',
+				'posts_per_page' => -1,
+				'meta_query' => array(
+					array(
+						'key' => 'people_category',
+						'value' => get_field('people_category'),
+						'compare' => '='
+					)
+				)
+			);
+			$query = new WP_Query($args);
+
+			if ($query->have_posts()) :
+				while ($query->have_posts()) : $query->the_post();
+					$title = get_field('position');
+					$name = get_the_title();
+					$emails = get_field('c');
+					$email = $emails[0]['email'] ?? '';
+					$research_interests = get_field('research_interests');
+			?>
+					<div class="list_item_row scrollin scrollinbottom">
+						<div class="list_item_col col2">
+							<div><?php echo esc_html($title); ?></div>
+						</div>
+						<div class="list_item_col text_c1 col2 text5">
+							<div><?php echo esc_html($name); ?></div>
+						</div>
+						<div class="list_item_col col8">
+							<div class="inner_list_item_col col6">
+								<div>
+									<?php if ($email): ?>
+										<a href="mailto:<?php echo esc_attr($email); ?>"><?php echo esc_html($email); ?></a>
+									<?php endif; ?>
+								</div>
+							</div>
+							<div class="inner_list_item_col col6">
+								<?php if ($research_interests): ?>
+									<div class="free_text">
+										<?php echo wp_kses_post($research_interests); ?>
+									</div>
+								<?php endif; ?>
+							</div>
+						</div>
+					</div>
+			<?php
+				endwhile;
+				wp_reset_postdata();
+			endif;
+			?>
+		</div>
+	</div>
+</div>
+
+<?php
+get_footer();
