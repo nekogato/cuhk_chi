373aafbd9d7e5f9315552c5f81d76226dc57e4ef
nicknpnp <126056970+nicknpnp@users.noreply.github.com>
2025-05-31 01:22:26 +0800

    update single project

--- single-research_project.php
+++ single-research_project.php
@@ -1,151 +1,211 @@
 <?php
+
 /**
+ * Template for displaying single research project
+ *
  * @package cuhk_chi
  */
 
 get_header();
 ?>
 
+<?php get_template_part('template-parts/roll-menu', null, array('target_page' => 'research-and-publication/research-projects')); ?>
 
 <?php
-while ( have_posts() ) :
+while (have_posts()) :
 	the_post();
-?>
-    <?php $project_category = get_the_terms(get_the_ID(),'project_category'); 
-    if ( $project_category ) {
-        if ( $project_category && ! is_wp_error( $project_category ) ) {
-            foreach ( $project_category as $term ) {
-                $termid = $term->term_id;
-                $termslug = $term->slug;
-                $termlink = get_term_link( $term );
-                if ( is_wp_error( $termlink ) ) {
-                    continue;
-                }
-                if(pll_current_language() == 'tc') {
-                    $termname = get_field('tc_name', 'project_category_' .$termid);
-                }elseif(pll_current_language() == 'sc'){
-                    $termname = get_field('sc_name', 'project_category_' .$termid);
-                }else{
-                    $termname = get_field('en_name', 'project_category_' .$termid);
-                };
-                ?>
-                <div>project_category: <?php echo $termname;?></div>
-                <?php
-            };
-                   
-        };
-    };
-    ?>
-    <?php if(get_field("project_title")): ?>
-        <div>project_title: <?php the_field("project_title"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("funding_start_year")): ?>
-        <div>funding_start_year: <?php the_field("funding_start_year"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("funding_end_year")): ?>
-        <div>funding_end_year: <?php the_field("funding_end_year"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("principal_investigator")): ?>
-        <div>principal_investigator: <?php the_field("principal_investigator"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("other_investigator")): ?>
-        <div>other_investigator: <?php the_field("other_investigator"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("granted_amount")): ?>
-        <div>granted_amount: <?php the_field("granted_amount"); ?></div>
-    <?php endif; ?>
-    <?php if(get_field("funding_organization")): ?>
-        <div>funding_organization: <?php the_field("funding_organization"); ?></div>
-    <?php endif; ?>
-    <?php
-    if( have_rows('flexible_content') ):
-		$i=0;?>
-        <div class="flexible_layout_wrapper ">
-            flexible_content:
-            <?php while ( have_rows('flexible_content') ) : the_row();
-                if( get_row_layout() == 'free_text' ):
-                    $freetext = get_sub_field("free_text");
-                    if($freetext){
-                    ?>
-                        <div class="flexible_layout flexible_layout_freetext scrollin scrollinbottom">
-                            <div class="free_text">
-                                <?php echo $freetext; ?>
-                            </div>
-                        </div>
-                    <?php
-                    };
-                elseif( get_row_layout() == 'single_image' ):
-                    $image = get_sub_field('image');
-                    $caption = get_sub_field('caption');
-                    if($image){
-                    ?>
-                        <div class="flexible_layout flexible_layout_photo scrollin scrollinleft">
-                            <div class="photo_wrapper">
-                                <div class="photo">
-                                    <img src="<?php echo esc_url($image['sizes']['l']); ?>" >
-                                </div>
-                                <?php if($caption){?>
-                                <div class="caption"><?php echo $caption;?></div>
-                                <?php };?>
-                            </div>
-                        </div>
-                    <?php
-                    };
-                elseif( get_row_layout() == 'video_upload' ):
-                    $video = get_sub_field('video_upload');
-                    if($video){
-                    ?>
-                        <div class="flexible_layout flexible_layout_video scrollin scrollinbottom">
-                            <div class="upload_video_wrapper">
-                                <video controls playsinline>
-                                    <source src="<?php echo $video["url"];?>" type="video/mp4">
-                                    Your browser does not support HTML5 video.
-                                </video>
-                            </div>
-                        </div>
-                    <?php
-                    };
-                elseif( get_row_layout() == 'slider' ):
-                    if( have_rows('image_list') ):
-					    $i++;
-                        ?>
-                        <div class="flexible_layout flexible_layout_slider scrollin scrollinbottom">
-                            <div class="swiper-container swiper">
-                                <div class="swiper-wrapper">
-                                    <?php
-                                    while( have_rows('image_list') ) : the_row();
-                                        $image = get_sub_field('image');
-                                        $caption = get_sub_field('caption');
-                                        ?>
-                                            <div class="swiper-slide">
-                                                <a href="<?php echo esc_url($image['sizes']['l']); ?>" class="photo" data-fancybox="gallery<?php echo $i ;?>" <?php if($caption){ ?>data-caption="<?php echo $caption; ?>"<?php }; ?>>
-                                                    <img src="<?php echo esc_url($image['sizes']['l']); ?>" >
-                                                </a>
-                                                <?php if($caption){ ?>
-                                                    <div class="caption"><?php echo $caption; ?></div>
-                                                <?php }; ?>
-                                            </div>
-                                        <?php
-                                    endwhile;
-                                    ?>
-                                </div>
-                            </div>
-                            <div class="prev_btn"></div>
-                            <div class="next_btn"></div>
-                        </div>
-                        <?php
-                    endif;
-                endif;
-            endwhile;?>
-        </div>
-    <?php
-    endif;
-    ?>
-
-<?php
-endwhile; // End of the loop.
+	$project_category = get_the_terms(get_the_ID(), 'project_category');
+	$project_title = get_field('project_title');
+	$funding_start_year = get_field('funding_start_year');
+	$funding_end_year = get_field('funding_end_year');
+	$principal_investigator = get_field('principal_investigator');
+	$other_investigator = get_field('other_investigator');
+	$granted_amount = get_field('granted_amount');
+	$funding_organization = get_field('funding_organization');
+	$banner_image = get_field('banner_image');
+	$banner_caption = get_field('banner_caption');
 ?>
 
+	<div class="section top_photo_banner_section banner_bg">
+		<div class="section_center_content small_section_center_content">
+			<div class="col_wrapper">
+				<div class="flex row">
+					<div class="col4 col">
+						<div class="col_spacing scrollin scrollinbottom">
+							<div class="text_wrapper vertical_text_wrapper">
+								<div class="text vertical_text">
+									<?php if ($project_category) : ?>
+										<h4 class="project_smalltitle">
+											<span><?php echo esc_html($project_category[0]->name); ?></span>
+										</h4>
+									<?php endif; ?>
+									<?php if ($project_title) : ?>
+										<h1 class="project_title">
+											<span><?php echo esc_html($project_title); ?></span>
+										</h1>
+									<?php endif; ?>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col8 col">
+						<div class="col_spacing scrollin scrollinleft">
+							<div class="photo_wrapper">
+								<?php if ($banner_image) : ?>
+									<div class="photo">
+										<img src="<?php echo esc_url($banner_image['url']); ?>" alt="<?php echo esc_attr($banner_image['alt']); ?>">
+									</div>
+								<?php endif; ?>
+								<?php if ($banner_caption) : ?>
+									<div class="caption"><?php echo esc_html($banner_caption); ?></div>
+								<?php endif; ?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="section section_left_right_content scrollin_p">
+		<div class="section_center_content small_section_center_content">
+			<div class="col_wrapper">
+				<div class="flex row">
+					<div class="col col4">
+						<div class="col_spacing scrollin scrollinbottom">
+							<div class="left_content free_text">
+								<?php if ($funding_start_year || $funding_end_year) : ?>
+									<h5><?php pll_e('Funding Period'); ?></h5>
+									<p>
+										<?php
+										if ($funding_start_year && $funding_end_year) {
+											echo esc_html($funding_start_year . ' - ' . $funding_end_year);
+										} elseif ($funding_start_year) {
+											echo esc_html($funding_start_year);
+										} elseif ($funding_end_year) {
+											echo esc_html($funding_end_year);
+										}
+										?>
+									</p>
+								<?php endif; ?>
+
+								<?php if ($principal_investigator) : ?>
+									<h5><?php pll_e('Principal Investigator'); ?></h5>
+									<p><?php echo esc_html($principal_investigator); ?></p>
+								<?php endif; ?>
+
+								<?php if ($other_investigator) : ?>
+									<h5><?php pll_e('Other Investigators'); ?></h5>
+									<p><?php echo esc_html($other_investigator); ?></p>
+								<?php endif; ?>
+
+								<?php if ($granted_amount) : ?>
+									<h5><?php pll_e('Granted Amount'); ?></h5>
+									<p><?php echo esc_html($granted_amount); ?></p>
+								<?php endif; ?>
+
+								<?php if ($funding_organization) : ?>
+									<h5><?php pll_e('Funding Organization'); ?></h5>
+									<p><?php echo esc_html($funding_organization); ?></p>
+								<?php endif; ?>
+							</div>
+						</div>
+					</div>
+					<div class="col col8">
+						<div class="col_spacing">
+							<div class="right_content">
+								<div class="flexible_layout_wrapper">
+									<?php
+									if (have_rows('flexible_content')) :
+										while (have_rows('flexible_content')) : the_row();
+											if (get_row_layout() == 'free_text') :
+												$freetext = get_sub_field('free_text');
+												if ($freetext) :
+									?>
+													<div class="flexible_layout flexible_layout_freetext scrollin scrollinbottom">
+														<div class="free_text">
+															<?php echo wp_kses_post($freetext); ?>
+														</div>
+													</div>
+												<?php
+												endif;
+											elseif (get_row_layout() == 'single_image') :
+												$image = get_sub_field('image');
+												$caption = get_sub_field('caption');
+												if ($image) :
+												?>
+													<div class="flexible_layout flexible_layout_photo scrollin scrollinleft">
+														<div class="photo_wrapper">
+															<div class="photo">
+																<img src="<?php echo esc_url($image['sizes']['l']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+															</div>
+															<?php if ($caption) : ?>
+																<div class="caption"><?php echo esc_html($caption); ?></div>
+															<?php endif; ?>
+														</div>
+													</div>
+												<?php
+												endif;
+											elseif (get_row_layout() == 'video_upload') :
+												$video = get_sub_field('video_upload');
+												if ($video) :
+												?>
+													<div class="flexible_layout flexible_layout_video scrollin scrollinbottom">
+														<div class="upload_video_wrapper">
+															<video controls playsinline>
+																<source src="<?php echo esc_url($video['url']); ?>" type="video/mp4">
+																<?php pll_e('Your browser does not support HTML5 video.'); ?>
+															</video>
+														</div>
+													</div>
+												<?php
+												endif;
+											elseif (get_row_layout() == 'slider') :
+												if (have_rows('image_list')) :
+													static $i = 0;
+													$i++;
+												?>
+													<div class="flexible_layout flexible_layout_slider scrollin scrollinbottom">
+														<div class="swiper-container swiper">
+															<div class="swiper-wrapper">
+																<?php
+																while (have_rows('image_list')) : the_row();
+																	$image = get_sub_field('image');
+																	$caption = get_sub_field('caption');
+																	if ($image) :
+																?>
+																		<div class="swiper-slide">
+																			<a href="<?php echo esc_url($image['sizes']['l']); ?>" class="photo" data-fancybox="gallery<?php echo $i; ?>" <?php if ($caption) : ?>data-caption="<?php echo esc_attr($caption); ?>" <?php endif; ?>>
+																				<img src="<?php echo esc_url($image['sizes']['l']); ?>" alt="<?php echo esc_attr($image['alt']); ?>">
+																			</a>
+																			<?php if ($caption) : ?>
+																				<div class="caption"><?php echo esc_html($caption); ?></div>
+																			<?php endif; ?>
+																		</div>
+																<?php
+																	endif;
+																endwhile;
+																?>
+															</div>
+														</div>
+														<div class="prev_btn"></div>
+														<div class="next_btn"></div>
+													</div>
+									<?php
+												endif;
+											endif;
+										endwhile;
+									endif;
+									?>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 <?php
+endwhile;
 get_footer();
