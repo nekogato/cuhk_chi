eb06f869d13a3bb37fa1f33b3bfd8d75badc6b85
nicknpnp <126056970+nicknpnp@users.noreply.github.com>
2025-06-05 08:22:54 +0800

    update

--- tmp-flexible.php
+++ tmp-flexible.php
@@ -17,75 +17,76 @@ get_header();
 while (have_posts()) :
 	the_post();
 	$page_title = get_the_title();
+	$page_description = get_field("introduction");
 ?>
 
 	<div class="section section_content admission_section scrollin scrollinbottom onscreen startani">
+		<div class="section_center_content small_section_center_content">
 
-
-		<?php if ($page_title) : ?>
-
-			<div class="section_center_content small_section_center_content">
+			<?php if ($page_title) : ?>
 				<h1 class="section_title text1 scrollin scrollinbottom"><?php echo esc_html($page_title); ?></h1>
-			</div>
+			<?php endif; ?>
+			<?php if ($page_description) : ?>
+				<div class="section_description scrollin scrollinbottom"><?php echo wp_kses_post($page_description); ?></div>
+			<?php endif; ?>
 
-		<?php endif; ?>
+			<?php if (have_rows('content_sections')) : ?>
+				<?php while (have_rows('content_sections')) : the_row(); ?>
 
-		<?php if (have_rows('content_sections')) : ?>
-			<?php while (have_rows('content_sections')) : the_row(); ?>
-
-				<?php if (get_row_layout() == 'free_text') : ?>
-					<?php $content = get_sub_field('content'); ?>
-					<?php if ($content) : ?>
-						<!-- Free Text Content Section -->
-						<div class="section_introduction scrollin scrollinbottom onscreen startani">
-							<div class="free_text">
-								<?php echo wp_kses_post($content); ?>
+					<?php if (get_row_layout() == 'free_text') : ?>
+						<?php $content = get_sub_field('content'); ?>
+						<?php if ($content) : ?>
+							<!-- Free Text Content Section -->
+							<div class="section_introduction scrollin scrollinbottom onscreen startani">
+								<div class="free_text">
+									<?php echo wp_kses_post($content); ?>
+								</div>
 							</div>
-						</div>
-					<?php endif; ?>
+						<?php endif; ?>
 
-				<?php elseif (get_row_layout() == 'expandable_section') : ?>
-					<?php
-					$introduction = get_sub_field('introduction');
-					$expandable_items = get_sub_field('expandable_items');
-					?>
-					<!-- Expandable FAQ/Admission Style Content Section -->
-					<div class="section_expandable_list">
-						<?php if ($expandable_items) : ?>
-							<?php $first_item = true; ?>
-							<?php foreach ($expandable_items as $item) : ?>
-								<?php
-								$question = $item['question'];
-								$answer = $item['answer'];
-								$is_active = $item['is_active'];
-								$active_class = ($is_active) ? ' active' : '';
-								$first_item = false;
-								?>
-								<div class="expandable_item<?php echo $active_class; ?>">
-									<div class="section_center_content small_section_center_content">
-										<div class="expandable_title text5"><?php echo esc_html($question); ?><div class="icon"></div>
-										</div>
-										<div class="hidden">
-											<div class="hidden_content">
-												<div class="free_text">
-													<?php echo wp_kses_post($answer); ?>
+					<?php elseif (get_row_layout() == 'expandable_section') : ?>
+						<?php
+						$introduction = get_sub_field('introduction');
+						$expandable_items = get_sub_field('expandable_items');
+						?>
+						<!-- Expandable FAQ/Admission Style Content Section -->
+						<div class="section_expandable_list">
+							<?php if ($expandable_items) : ?>
+								<?php $first_item = true; ?>
+								<?php foreach ($expandable_items as $item) : ?>
+									<?php
+									$question = $item['question'];
+									$answer = $item['answer'];
+									$is_active = $item['is_active'];
+									$active_class = ($is_active) ? ' active' : '';
+									$first_item = false;
+									?>
+									<div class="expandable_item<?php echo $active_class; ?>">
+										<div class="section_center_content small_section_center_content">
+											<div class="expandable_title text5"><?php echo esc_html($question); ?><div class="icon"></div>
+											</div>
+											<div class="hidden">
+												<div class="hidden_content">
+													<div class="free_text">
+														<?php echo wp_kses_post($answer); ?>
+													</div>
 												</div>
 											</div>
 										</div>
 									</div>
-								</div>
-							<?php endforeach; ?>
-						<?php endif; ?>
-					</div>
+								<?php endforeach; ?>
+							<?php endif; ?>
+						</div>
 
-				<?php endif; ?>
+					<?php endif; ?>
 
-			<?php endwhile; ?>
-		<?php endif; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
 
-	<?php
-endwhile;
-	?>
+		<?php
+	endwhile;
+		?>
+		</div>
 	</div>
 
 	<?php
