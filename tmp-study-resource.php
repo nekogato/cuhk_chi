b02ce55a1a98f02184de76db25b19cd898cbdf3d
nekogato <05kcwong@gmail.com>
2025-06-13 12:09:56 +0800

    Update tmp-study-resource.php

--- tmp-study-resource.php
+++ tmp-study-resource.php
@@ -13,7 +13,7 @@ if (have_posts()) :
 	while (have_posts()) : the_post();
 
 		// Get ACF fields
-		$resources_title = get_the_title();
+		$resources_title = get_field('page_title');
 		$resources_description = get_field('introduction');
 		$resources_introduction = get_field('resources_introduction');
 		$download_section_title = get_field('download_section_title');
@@ -22,11 +22,11 @@ if (have_posts()) :
 		<div class="section section_content resource_top_section">
 			<div class="section_center_content small_section_center_content">
 				<?php if ($resources_title): ?>
-					<h1 class="section_title text1 scrollin scrollinbottom"><?php echo esc_html($resources_title); ?></h1>
+					<h1 class="section_title text1 scrollin scrollinbottom"><?php echo wp_kses_post($resources_title); ?></h1>
 				<?php endif; ?>
 
 				<?php if ($resources_description): ?>
-					<div class="section_description scrollin scrollinbottom col6"><?php echo esc_html($resources_description); ?></div>
+					<div class="section_description scrollin scrollinbottom col6"><?php echo wp_kses_post($resources_description); ?></div>
 				<?php endif; ?>
 
 				<?php if ($resources_introduction): ?>
@@ -40,51 +40,20 @@ if (have_posts()) :
 		</div>
 
 		<div class="section section_content filter_menu_section resource_filter_menu_section scrollin scrollinbottom">
-			<div class="section_center_content small_section_center_content scrollin scrollinbottom">
-				<?php if ($download_section_title): ?>
-					<h2 class="section_title text1 scrollin scrollinbottom"><?php echo esc_html($download_section_title); ?></h2>
-				<?php endif; ?>
-			</div>
+			
 
-			<div class="filter_menu_wrapper">
-				<div class="filter_menu filter_menu_left_bg section_center_content small_section_center_content scrollin scrollinbottom">
-					<div class="filter_menu_content">
-						<div class="filter_checkbox_wrapper text7 filter_switchable_wrapper">
-							<?php if (have_rows('filter_categories')): ?>
-								<?php
-								$filter_index = 1;
-								while (have_rows('filter_categories')): the_row();
-									$category_name = get_sub_field('category_name');
-									$filter_id = 'filter' . $filter_index;
-									$is_first = ($filter_index === 1);
-								?>
-									<div class="filter_checkbox">
-										<div class="checkbox">
-											<input name="filter" type="radio" id="<?php echo esc_attr($filter_id); ?>" <?php echo $is_first ? 'checked' : ''; ?>>
-											<label for="<?php echo esc_attr($filter_id); ?>">
-												<span><?php echo esc_html($category_name); ?></span>
-											</label>
-										</div>
-									</div>
-								<?php
-									$filter_index++;
-								endwhile;
-								?>
-							<?php endif; ?>
-						</div>
-					</div>
-				</div>
-			</div>
-
-			<div class="switchable_section_expandable_list_wrapper scrollin scrollinbottom">
 				<?php if (have_rows('filter_categories')): ?>
 					<?php
-					$category_index = 1;
 					while (have_rows('filter_categories')): the_row();
-						$filter_id = 'filter' . $category_index;
-						$is_first = ($category_index === 1);
+						$category_name = get_sub_field('category_name');
 					?>
-						<div class="section_expandable_list switchable_section_expandable_list <?php echo $is_first ? 'active' : ''; ?>" data-id="<?php echo esc_attr($filter_id); ?>">
+						<div class="section_center_content small_section_center_content scrollin scrollinbottom">
+							<?php if ($category_name): ?>
+								<h2 class="section_title text1 scrollin scrollinbottom"><?php echo wp_kses_post($category_name); ?></h2>
+							<?php endif; ?>
+						</div>
+
+						<div class="section_expandable_list" >
 							<?php if (have_rows('download_groups')): ?>
 								<?php while (have_rows('download_groups')): the_row(); ?>
 									<?php
@@ -93,7 +62,7 @@ if (have_posts()) :
 									?>
 									<div class="expandable_item">
 										<div class="section_center_content small_section_center_content">
-											<div class="expandable_title text5"><?php echo esc_html($group_title); ?><div class="icon"></div>
+											<div class="expandable_title text5"><?php echo wp_kses_post($group_title); ?><div class="icon"></div>
 											</div>
 											<div class="hidden">
 												<div class="hidden_content">
@@ -108,11 +77,9 @@ if (have_posts()) :
 							<?php endif; ?>
 						</div>
 					<?php
-						$category_index++;
 					endwhile;
 					?>
 				<?php endif; ?>
-			</div>
 		</div>
 
 <?php
