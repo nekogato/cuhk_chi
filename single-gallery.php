efaddf094fd1e64bde0972e030e84596fb89a63d
nicknpnp <126056970+nicknpnp@users.noreply.github.com>
2025-06-14 08:33:46 +0800

    update gallery

--- single-gallery.php
+++ single-gallery.php
@@ -3,49 +3,12 @@
 Template for single gallery posts
 */
 
-get_header(); ?>
+get_header();
 
-<div class="section roll_menu_section sticky_section">
-	<div class="roll_menu scrollin scrollinbottom">
-		<div class="roll_top_menu text7">
-			<div class="section_center_content">
-				<div class="swiper-container swiper">
-					<div class="swiper-wrapper">
-						<div class="swiper-slide">
-							<div><a href="#" class="active"><?php echo cuhk_multilang_text("新聞與活動", "新闻与活动", "News & Events"); ?></a></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="roll_bottom_menu text7">
-			<div class="section_center_content">
-				<div class="swiper-container swiper">
-					<div class="swiper-wrapper">
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("公告", "公告", "Announcements"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("即將舉行的活動", "即将举行的活动", "Up Coming Events"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("新聞", "新闻", "News"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("學系新聞", "学系新闻", "Department in the News"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("通訊", "通讯", "Newsletter"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#" class="active"><?php echo cuhk_multilang_text("相片集", "相片集", "Media Gallery"); ?></a></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+// Include the roll menu template part
+get_template_part('template-parts/roll-menu', null, array(
+	'target_page' => 'media-coverage/gallery-index' // Assuming this is the parent page slug
+)); ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
--- tmp-gallery.php
+++ tmp-gallery.php
@@ -3,49 +3,10 @@
 Template Name: Gallery Archive
 */
 
-get_header(); ?>
+get_header();
 
-<div class="section roll_menu_section sticky_section">
-	<div class="roll_menu scrollin scrollinbottom">
-		<div class="roll_top_menu text7">
-			<div class="section_center_content">
-				<div class="swiper-container swiper">
-					<div class="swiper-wrapper">
-						<div class="swiper-slide">
-							<div><a href="#" class="active"><?php echo cuhk_multilang_text("新聞與活動", "新闻与活动", "News & Events"); ?></a></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="roll_bottom_menu text7">
-			<div class="section_center_content">
-				<div class="swiper-container swiper">
-					<div class="swiper-wrapper">
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("公告", "公告", "Announcements"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("即將舉行的活動", "即将举行的活动", "Up Coming Events"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("新聞", "新闻", "News"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("學系新聞", "学系新闻", "Department in the News"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#"><?php echo cuhk_multilang_text("通訊", "通讯", "Newsletter"); ?></a></div>
-						</div>
-						<div class="swiper-slide">
-							<div><a href="#" class="active"><?php echo cuhk_multilang_text("相片集", "相片集", "Media Gallery"); ?></a></div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+// Include the roll menu template part
+get_template_part('template-parts/roll-menu'); ?>
 
 <div x-data="galleryFilter()">
 	<div class="section section_content filter_menu_section">
