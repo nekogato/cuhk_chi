59530bfd56d3c5b35e0671d5b309b1ef83b775ed
nekogato <05kcwong@gmail.com>
2025-08-26 11:09:55 +0800

    Update single-profile.php

--- single-profile.php
+++ single-profile.php
@@ -30,17 +30,15 @@ $parent_slug = '';
 if ($people_categories && !is_wp_error($people_categories)) {
 	foreach ($people_categories as $term) {
 		// Get the direct parent if it exists
-		if ($term->parent != 0) {
 			$parent_term = get_term($term->parent, 'people_category');
 			if ($parent_term && !is_wp_error($parent_term)) {
 				$parent_slug = $parent_term->slug;
 			}
-		}
 
 		break; // stop after first category
 	}
 }
-var_dump( $people_categories, $parent_slug );
+var_dump( "slug:", $parent_slug );
 // Include roll menu
 get_template_part('template-parts/roll-menu', null, array('target_page' => $target_page));
 
