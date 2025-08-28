413fc41c70e431d49a72abd38061d1cb09c56777
nekogato <05kcwong@gmail.com>
2025-08-28 13:44:49 +0800

    update about

--- tmp-about_chair.php
+++ tmp-about_chair.php
@@ -26,44 +26,46 @@ get_header();
 </div>
 <div class="section top_photo_banner_section banner_bg2">
 	<div class="section_center_content small_section_center_content">
-		<div class="col_wrapper">
-			<div class="flex row">
-				<div class="col3 col">
-					<div class="col_spacing scrollin scrollinbottom">
-						<div class="text_wrapper vertical_text_wrapper absolute_vertical_text_wrapper">
-							<div class="text vertical_text">
-								<?php if (get_field("small_headline")): ?>
-									<h4 class="project_smalltitle"><span><?php the_field("small_headline"); ?></span></h4>
-								<?php endif; ?>
-								<?php if (get_field("headline")): ?>
-									<h1 class="project_title no_max_height"><span><?php the_field("headline"); ?></span></h1>
-								<?php endif; ?>
+		<div class="col10 center_content">
+			<div class="col_wrapper">
+				<div class="flex row">
+					<div class="col3 col">
+						<div class="col_spacing scrollin scrollinbottom">
+							<div class="text_wrapper vertical_text_wrapper absolute_vertical_text_wrapper">
+								<div class="text vertical_text">
+									<?php if (get_field("small_headline")): ?>
+										<h4 class="project_smalltitle"><span><?php the_field("small_headline"); ?></span></h4>
+									<?php endif; ?>
+									<?php if (get_field("headline")): ?>
+										<h1 class="project_title no_max_height"><span><?php the_field("headline"); ?></span></h1>
+									<?php endif; ?>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col9 col">
-					<div class="col_spacing scrollin scrollinleft">
-						<div class="chair_photo_wrapper">
-							<?php
-							$chair_image = get_field("chair_image");
-							if ($chair_image): ?>
-								<div class="photo_wrapper col10">
-									<div class="photo">
-										<img src="<?php echo esc_url($chair_image['sizes']['l']); ?>">
+					<div class="col9 col">
+						<div class="col_spacing scrollin scrollinleft">
+							<div class="chair_photo_wrapper">
+								<?php
+								$chair_image = get_field("chair_image");
+								if ($chair_image): ?>
+									<div class="photo_wrapper col10">
+										<div class="photo">
+											<img src="<?php echo esc_url($chair_image['sizes']['l']); ?>">
+										</div>
+										<?php if (get_field("chair_image_caption")): ?>
+											<div class="caption"><?php the_field("chair_image_caption"); ?></div>
+										<?php endif; ?>
 									</div>
-									<?php if (get_field("chair_image_caption")): ?>
-										<div class="caption"><?php the_field("chair_image_caption"); ?></div>
+								<?php endif; ?>
+								<div class="chair_name">
+									<?php if (get_field("chair_name")): ?>
+										<div class="t1 text5"><?php the_field("chair_name"); ?></div>
+									<?php endif; ?>
+									<?php if (get_field("chair_title")): ?>
+										<div class="t2"><?php the_field("chair_title"); ?></div>
 									<?php endif; ?>
 								</div>
-							<?php endif; ?>
-							<div class="chair_name">
-								<?php if (get_field("chair_name")): ?>
-									<div class="t1 text5"><?php the_field("chair_name"); ?></div>
-								<?php endif; ?>
-								<?php if (get_field("chair_title")): ?>
-									<div class="t2"><?php the_field("chair_title"); ?></div>
-								<?php endif; ?>
 							</div>
 						</div>
 					</div>
@@ -75,61 +77,63 @@ get_header();
 
 <div class="section section_left_right_content scrollin_p">
 	<div class="section_center_content small_section_center_content">
-		<div class="col_wrapper">
-			<div class="flex row">
-				<div class="col7 col">
-					<div class="col_spacing scrollin scrollinbottom">
-						<?php if (get_field("left_free_text")): ?>
-							<div class="free_text">
-								<?php the_field("left_free_text"); ?>
-							</div>
-						<?php endif; ?>
+		<div class="col10 center_content">
+			<div class="col_wrapper">
+				<div class="flex row">
+					<div class="col7 col">
+						<div class="col_spacing scrollin scrollinbottom">
+							<?php if (get_field("left_free_text")): ?>
+								<div class="free_text">
+									<?php the_field("left_free_text"); ?>
+								</div>
+							<?php endif; ?>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 
-		<div class="col_wrapper">
-			<div class="flex row">
-				<div class="col col5"></div>
-				<div class="col col7">
-					<div class="col_spacing">
-						<div class="right_content">
-							<?php
-							if (have_rows('flexible_content')):
-								while (have_rows('flexible_content')): the_row();
-									if (get_row_layout() == 'free_text'):
-										$freetext = get_sub_field("free_text");
-										if ($freetext):
-							?>
-											<div class="flexible_layout flexible_layout_freetext scrollin scrollinbottom">
-												<div class="free_text">
-													<?php echo $freetext; ?>
-												</div>
-											</div>
-										<?php
-										endif;
-									elseif (get_row_layout() == 'single_image'):
-										$image = get_sub_field('image');
-										$caption = get_sub_field('caption');
-										if ($image):
-										?>
-											<div class="flexible_layout flexible_layout_photo scrollin scrollinbottom">
-												<div class="photo_wrapper">
-													<div class="photo">
-														<img src="<?php echo esc_url($image['sizes']['l']); ?>">
+			<div class="col_wrapper">
+				<div class="flex row">
+					<div class="col col5"></div>
+					<div class="col col7">
+						<div class="col_spacing">
+							<div class="right_content">
+								<?php
+								if (have_rows('flexible_content')):
+									while (have_rows('flexible_content')): the_row();
+										if (get_row_layout() == 'free_text'):
+											$freetext = get_sub_field("free_text");
+											if ($freetext):
+								?>
+												<div class="flexible_layout flexible_layout_freetext scrollin scrollinbottom">
+													<div class="free_text">
+														<?php echo $freetext; ?>
 													</div>
-													<?php if ($caption): ?>
-														<div class="caption"><?php echo $caption; ?></div>
-													<?php endif; ?>
 												</div>
-											</div>
-							<?php
+											<?php
+											endif;
+										elseif (get_row_layout() == 'single_image'):
+											$image = get_sub_field('image');
+											$caption = get_sub_field('caption');
+											if ($image):
+											?>
+												<div class="flexible_layout flexible_layout_photo scrollin scrollinbottom">
+													<div class="photo_wrapper">
+														<div class="photo">
+															<img src="<?php echo esc_url($image['sizes']['l']); ?>">
+														</div>
+														<?php if ($caption): ?>
+															<div class="caption"><?php echo $caption; ?></div>
+														<?php endif; ?>
+													</div>
+												</div>
+								<?php
+											endif;
 										endif;
-									endif;
-								endwhile;
-							endif;
-							?>
+									endwhile;
+								endif;
+								?>
+							</div>
 						</div>
 					</div>
 				</div>
--- tmp-about_song.php
+++ tmp-about_song.php
@@ -19,101 +19,103 @@ get_header();
 	<div class="main_song_content">
 		<div class="main_song_wrapper scrollin scrollinbottom">
 			<div class="section_center_content small_section_center_content">
-				<?php if (have_rows('song_repeater')) : ?>
-					<div class="main_song_btn_wrapper">
-						<ul>
-							<?php
-							$firsttext;
-							$first = true;
-							while (have_rows('song_repeater')) : the_row();
-								$version_name = get_sub_field('version_name');
-								$song_file = get_sub_field('song_file');
-								$firsttext = $version_name;
-								if ($song_file) :
-							?>
-									<li>
-										<a href="#" data-song="<?php echo esc_url($song_file['url']); ?>" class="song_btn <?php echo $first ? 'active' : ''; ?>">
-											<?php echo esc_html($version_name); ?>
-										</a>
-									</li>
-							<?php
-									$first = false;
-								endif;
-							endwhile;
-							?>
-						</ul>
-					</div>
-				<?php endif; ?>
-
-				<div class="main_song">
-					<?php
-					$song_thumb = get_field('song_thumb');
-					if ($song_thumb) :
-					?>
-						<div class="song_thumb">
-							<a href="<?php echo esc_url($song_thumb['url']); ?>" data-fancybox>
-								<img src="<?php echo esc_url($song_thumb['sizes']['xl']); ?>" alt="<?php echo esc_attr($song_thumb['alt']); ?>">
-							</a>
+				<div class="col10 center_content">
+					<?php if (have_rows('song_repeater')) : ?>
+						<div class="main_song_btn_wrapper">
+							<ul>
+								<?php
+								$firsttext;
+								$first = true;
+								while (have_rows('song_repeater')) : the_row();
+									$version_name = get_sub_field('version_name');
+									$song_file = get_sub_field('song_file');
+									$firsttext = $version_name;
+									if ($song_file) :
+								?>
+										<li>
+											<a href="#" data-song="<?php echo esc_url($song_file['url']); ?>" class="song_btn <?php echo $first ? 'active' : ''; ?>">
+												<?php echo esc_html($version_name); ?>
+											</a>
+										</li>
+								<?php
+										$first = false;
+									endif;
+								endwhile;
+								?>
+							</ul>
 						</div>
 					<?php endif; ?>
 
-					<div class="song_text">
-						<div class="song_text_top">
-							<div class="name">
-								<?php if (get_field('song_small_title')) : ?>
-									<div class="t1 text5"><?php the_field('song_small_title'); ?></div>
-								<?php endif; ?>
-								<?php if (get_field('song_name')) : ?>
-									<div class="t2 text2"><?php the_field('song_name'); ?> - <span class="version_name"><?php echo $firsttext;?></span></div>
+					<div class="main_song">
+						<?php
+						$song_thumb = get_field('song_thumb');
+						if ($song_thumb) :
+						?>
+							<div class="song_thumb">
+								<a href="<?php echo esc_url($song_thumb['url']); ?>" data-fancybox>
+									<img src="<?php echo esc_url($song_thumb['sizes']['xl']); ?>" alt="<?php echo esc_attr($song_thumb['alt']); ?>">
+								</a>
+							</div>
+						<?php endif; ?>
+
+						<div class="song_text">
+							<div class="song_text_top">
+								<div class="name">
+									<?php if (get_field('song_small_title')) : ?>
+										<div class="t1 text5"><?php the_field('song_small_title'); ?></div>
+									<?php endif; ?>
+									<?php if (get_field('song_name')) : ?>
+										<div class="t2 text2"><?php the_field('song_name'); ?> - <span class="version_name"><?php echo $firsttext;?></span></div>
+									<?php endif; ?>
+								</div>
+								<?php if (get_field('song_short_info')) : ?>
+									<div class="info"><?php the_field('song_short_info'); ?></div>
 								<?php endif; ?>
 							</div>
-							<?php if (get_field('song_short_info')) : ?>
-								<div class="info"><?php the_field('song_short_info'); ?></div>
-							<?php endif; ?>
-						</div>
 
-						<div class="song_player_wrapper">
-							<div class="audio-player">
-								<table>
-									<tbody>
-										<tr>
-											<td class="play-btn-td">
-												<div class="play-btn"></div>
-												<div class="pause-btn active"></div>
-											</td>
-											<td>
-												<div class="audio-wrapper player-container" href="javascript:;">
-													<audio class="player">
-														<?php
-														if (have_rows('song_repeater')) :
-															while (have_rows('song_repeater')) : the_row();
-																$song_file = get_sub_field('song_file');
-																if ($song_file) :
-														?>
-																	<source src="<?php echo esc_url($song_file['url']); ?>" type="audio/mp3">
-														<?php
-																	break;
-																endif;
-															endwhile;
-														endif;
-														?>
-													</audio>
-												</div>
-												<div class="player-controls scrubber">
-													<div class="seekObjContainer">
-														<progress class="seekObj" value="0" max="1"></progress>
+							<div class="song_player_wrapper">
+								<div class="audio-player">
+									<table>
+										<tbody>
+											<tr>
+												<td class="play-btn-td">
+													<div class="play-btn"></div>
+													<div class="pause-btn active"></div>
+												</td>
+												<td>
+													<div class="audio-wrapper player-container" href="javascript:;">
+														<audio class="player">
+															<?php
+															if (have_rows('song_repeater')) :
+																while (have_rows('song_repeater')) : the_row();
+																	$song_file = get_sub_field('song_file');
+																	if ($song_file) :
+															?>
+																		<source src="<?php echo esc_url($song_file['url']); ?>" type="audio/mp3">
+															<?php
+																		break;
+																	endif;
+																endwhile;
+															endif;
+															?>
+														</audio>
 													</div>
-													<div class="pbg"></div>
-													<div class="pbar"></div>
-												</div>
-											</td>
-											<td class="time-td">
-												<span class="start-time">00:00</span> / <span class="end-time">00:00</span>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-								<div class="loader"></div>
+													<div class="player-controls scrubber">
+														<div class="seekObjContainer">
+															<progress class="seekObj" value="0" max="1"></progress>
+														</div>
+														<div class="pbg"></div>
+														<div class="pbar"></div>
+													</div>
+												</td>
+												<td class="time-td">
+													<span class="start-time">00:00</span> / <span class="end-time">00:00</span>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+									<div class="loader"></div>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -125,60 +127,64 @@ get_header();
 	<?php if (get_field('section_name') || get_field('section_description')) : ?>
 		<div class="song_section_intro scrollin scrollinbottom">
 			<div class="section_center_content small_section_center_content">
-				<?php if (get_field('section_name')) : ?>
-					<h1 class="section_title text1 scrollin scrollinbottom"><?php echo wp_kses_post(get_field('section_name')); ?></h1>
-				<?php endif; ?>
-				<?php if (get_field('section_description')) : ?>
-					<div class="section_description scrollin scrollinbottom col6"><?php echo wp_kses_post(get_field('section_description')); ?></div>
-				<?php endif; ?>
+				<div class="col10 center_content">
+					<?php if (get_field('section_name')) : ?>
+						<h1 class="section_title text1 scrollin scrollinbottom"><?php echo wp_kses_post(get_field('section_name')); ?></h1>
+					<?php endif; ?>
+					<?php if (get_field('section_description')) : ?>
+						<div class="section_description scrollin scrollinbottom col6"><?php echo wp_kses_post(get_field('section_description')); ?></div>
+					<?php endif; ?>
+				</div>
 			</div>
 		</div>
 	<?php endif; ?>
 
 	<div class="song_section_text">
 		<div class="section_center_content small_section_center_content scrollin scrollinbottom">
-			<?php if (get_field('song_info')) : ?>
-				<div class="basic_description free_text" style="color:#6f6ea6">
-					<?php echo wp_kses_post(get_field('song_info')); ?>
-				</div>
-			<?php endif; ?>
+			<div class="col10 center_content">
+				<?php if (get_field('song_info')) : ?>
+					<div class="basic_description free_text" style="color:#6f6ea6">
+						<?php echo wp_kses_post(get_field('song_info')); ?>
+					</div>
+				<?php endif; ?>
 
-			<div class="col_wrapper">
-				<div class="flex row">
-					<div class="col col5">
-						<div class="col_spacing">
-							<div class="left_content free_text" style="color:#6f6ea6">
-								<?php if (get_field('song_lyrics')) : ?>
-									<?php echo wp_kses_post(get_field('song_lyrics')); ?>
-								<?php endif; ?>
+				<div class="col_wrapper">
+					<div class="flex row">
+						<div class="col col5">
+							<div class="col_spacing">
+								<div class="left_content free_text" style="color:#6f6ea6">
+									<?php if (get_field('song_lyrics')) : ?>
+										<?php echo wp_kses_post(get_field('song_lyrics')); ?>
+									<?php endif; ?>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="col col6">
-						<div class="col_spacing">
-							<div class="right_content">
-								<div class="flexible_layout_wrapper">
-									<?php
-									$song_img = get_field('song_img');
-									if ($song_img) :
-									?>
-										<div class="flexible_layout flexible_layout_photo scrollin scrollinbottom">
-											<div class="photo">
-												<img src="<?php echo esc_url($song_img['sizes']['xl']); ?>" alt="<?php echo esc_attr($song_img['alt']); ?>">
+						<div class="col col6">
+							<div class="col_spacing">
+								<div class="right_content">
+									<div class="flexible_layout_wrapper">
+										<?php
+										$song_img = get_field('song_img');
+										if ($song_img) :
+										?>
+											<div class="flexible_layout flexible_layout_photo scrollin scrollinbottom">
+												<div class="photo">
+													<img src="<?php echo esc_url($song_img['sizes']['xl']); ?>" alt="<?php echo esc_attr($song_img['alt']); ?>">
+												</div>
+												<?php if (get_field('song_img_caption')) : ?>
+													<div class="caption"><?php echo wp_kses_post(get_field('song_img_caption')); ?></div>
+												<?php endif; ?>
 											</div>
-											<?php if (get_field('song_img_caption')) : ?>
-												<div class="caption"><?php echo wp_kses_post(get_field('song_img_caption')); ?></div>
-											<?php endif; ?>
-										</div>
-									<?php endif; ?>
+										<?php endif; ?>
 
-									<?php if (get_field('song_remark')) : ?>
-										<div class="flexible_layout flexible_layout_freetext scrollin scrollinbottom">
-											<div class="free_text text8">
-												<?php echo wp_kses_post(get_field('song_remark')); ?>
+										<?php if (get_field('song_remark')) : ?>
+											<div class="flexible_layout flexible_layout_freetext scrollin scrollinbottom">
+												<div class="free_text text8">
+													<?php echo wp_kses_post(get_field('song_remark')); ?>
+												</div>
 											</div>
-										</div>
-									<?php endif; ?>
+										<?php endif; ?>
+									</div>
 								</div>
 							</div>
 						</div>
