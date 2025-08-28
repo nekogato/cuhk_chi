cb75b16ebe9d2d5a0e587fa38d259578d8d92c89
nekogato <05kcwong@gmail.com>
2025-08-28 14:09:08 +0800

    Update single-profile.php

--- single-profile.php
+++ single-profile.php
@@ -77,102 +77,103 @@ if (have_posts()) :
 
 		<div class="section section_content people_detail_section">
 			<div class="section_center_content small_section_center_content">
+				<div class="col10 center_content">
+					<div class="people_detail_content">
+						<div class="back_btn_wrapper scrollin scrollinbottom">
+							<a href="<?php echo pll_get_page_url("people/teaching-staff")."?people_category=".$parent_slug ?>" class="back_btn"><?php echo cuhk_multilang_text("返回","","Back"); ?></a>
+						</div>
+						<div class="people_detail_incontent">
+							<div class="people_detail_photo_wrapper scrollin scrollinbottom">
+								<div class="people_detail_photo">
+									<?php if ($photo): ?>
+										<img src="<?php echo esc_url($photo['sizes']['large']); ?>"
+											alt="<?php echo esc_attr($photo['alt']); ?>">
+									<?php endif; ?>
+								</div>
+							</div>
+							<div class="people_detail_text scrollin scrollinbottom">
+								<div class="name text4"><?php the_title(); ?></div>
+								<?php if ($position): ?>
+									<div class="position text5"><?php echo wp_kses_post($position); ?></div>
+								<?php endif; ?>
 
-				<div class="people_detail_content">
-					<div class="back_btn_wrapper scrollin scrollinbottom">
-						<a href="<?php echo pll_get_page_url("people/teaching-staff")."?people_category=".$parent_slug ?>" class="back_btn"><?php echo cuhk_multilang_text("返回","","Back"); ?></a>
-					</div>
-					<div class="people_detail_incontent">
-						<div class="people_detail_photo_wrapper scrollin scrollinbottom">
-							<div class="people_detail_photo">
-								<?php if ($photo): ?>
-									<img src="<?php echo esc_url($photo['sizes']['large']); ?>"
-										alt="<?php echo esc_attr($photo['alt']); ?>">
+								<?php if ($qualifications): ?>
+									<div class="qualifications text6">
+										<ul>
+											<?php
+											$qualification_items = explode(';', $qualifications);
+											foreach ($qualification_items as $item):
+												$item = trim($item);
+												if (!empty($item)):
+											?>
+													<li><?php echo esc_html($item); ?></li>
+											<?php
+												endif;
+											endforeach;
+											?>
+										</ul>
+									</div>
+								<?php endif; ?>
+
+								<div class="info_table text6">
+									<div class="table_flex_item_wrapper">
+										<?php if ($emails): ?>
+											<div class="table_flex_item">
+												<div class="title text7"><?php echo cuhk_multilang_text("電郵","","Email"); ?></div>
+												<div class="text text6">
+													<?php if (is_array($emails)): ?>
+														<?php foreach ($emails as $email): ?>
+															<a href="mailto:<?php echo esc_attr($email['email']); ?>">
+																<?php echo esc_html($email['email']); ?>
+															</a><br>
+														<?php endforeach; ?>
+													<?php endif; ?>
+												</div>
+											</div>
+										<?php endif; ?>
+
+										<?php if ($phones): ?>
+											<div class="table_flex_item">
+												<div class="title text7"><?php echo cuhk_multilang_text("電話","","Tel"); ?></div>
+												<div class="text text6">
+													<?php if (is_array($phones)): ?>
+														<?php foreach ($phones as $phone): ?>
+															<?php echo esc_html($phone['phone']); ?><br>
+														<?php endforeach; ?>
+													<?php endif; ?>
+												</div>
+											</div>
+										<?php endif; ?>
+
+										<?php if ($faxes): ?>
+											<div class="table_flex_item">
+												<div class="title text7"><?php echo cuhk_multilang_text("傳真","","Fax"); ?></div>
+												<div class="text text6">
+													<?php if (is_array($faxes)): ?>
+														<?php foreach ($faxes as $fax): ?>
+															<?php echo esc_html($fax['fax']); ?><br>
+														<?php endforeach; ?>
+													<?php endif; ?>
+												</div>
+											</div>
+										<?php endif; ?>
+
+										<?php if ($address): ?>
+											<div class="table_flex_item">
+												<div class="title text7"><?php echo cuhk_multilang_text("地址","","Address"); ?></div>
+												<div class="text text6"><?php echo esc_html($address); ?></div>
+											</div>
+										<?php endif; ?>
+									</div>
+								</div>
+
+								<?php if ($description): ?>
+									<div class="description">
+										<div class="t1 text7"><?php echo cuhk_multilang_text("簡介","","Description"); ?></div>
+										<div class="t2 free_text"><?php echo wp_kses_post($description); ?></div>
+									</div>
 								<?php endif; ?>
 							</div>
-						</div>
-						<div class="people_detail_text scrollin scrollinbottom">
-							<div class="name text4"><?php the_title(); ?></div>
-							<?php if ($position): ?>
-								<div class="position text5"><?php echo wp_kses_post($position); ?></div>
-							<?php endif; ?>
-
-							<?php if ($qualifications): ?>
-								<div class="qualifications text6">
-									<ul>
-										<?php
-										$qualification_items = explode(';', $qualifications);
-										foreach ($qualification_items as $item):
-											$item = trim($item);
-											if (!empty($item)):
-										?>
-												<li><?php echo esc_html($item); ?></li>
-										<?php
-											endif;
-										endforeach;
-										?>
-									</ul>
-								</div>
-							<?php endif; ?>
-
-							<div class="info_table text6">
-								<div class="table_flex_item_wrapper">
-									<?php if ($emails): ?>
-										<div class="table_flex_item">
-											<div class="title text7"><?php echo cuhk_multilang_text("電郵","","Email"); ?></div>
-											<div class="text text6">
-												<?php if (is_array($emails)): ?>
-													<?php foreach ($emails as $email): ?>
-														<a href="mailto:<?php echo esc_attr($email['email']); ?>">
-															<?php echo esc_html($email['email']); ?>
-														</a><br>
-													<?php endforeach; ?>
-												<?php endif; ?>
-											</div>
-										</div>
-									<?php endif; ?>
-
-									<?php if ($phones): ?>
-										<div class="table_flex_item">
-											<div class="title text7"><?php echo cuhk_multilang_text("電話","","Tel"); ?></div>
-											<div class="text text6">
-												<?php if (is_array($phones)): ?>
-													<?php foreach ($phones as $phone): ?>
-														<?php echo esc_html($phone['phone']); ?><br>
-													<?php endforeach; ?>
-												<?php endif; ?>
-											</div>
-										</div>
-									<?php endif; ?>
-
-									<?php if ($faxes): ?>
-										<div class="table_flex_item">
-											<div class="title text7"><?php echo cuhk_multilang_text("傳真","","Fax"); ?></div>
-											<div class="text text6">
-												<?php if (is_array($faxes)): ?>
-													<?php foreach ($faxes as $fax): ?>
-														<?php echo esc_html($fax['fax']); ?><br>
-													<?php endforeach; ?>
-												<?php endif; ?>
-											</div>
-										</div>
-									<?php endif; ?>
-
-									<?php if ($address): ?>
-										<div class="table_flex_item">
-											<div class="title text7"><?php echo cuhk_multilang_text("地址","","Address"); ?></div>
-											<div class="text text6"><?php echo esc_html($address); ?></div>
-										</div>
-									<?php endif; ?>
-								</div>
-							</div>
-
-							<?php if ($description): ?>
-								<div class="description">
-									<div class="t1 text7"><?php echo cuhk_multilang_text("簡介","","Description"); ?></div>
-									<div class="t2 free_text"><?php echo wp_kses_post($description); ?></div>
-								</div>
-							<?php endif; ?>
 						</div>
 					</div>
 				</div>
@@ -182,12 +183,14 @@ if (have_posts()) :
 				<?php if ($research_interests): ?>
 					<div class="expandable_item">
 						<div class="section_center_content small_section_center_content">
-							<div class="expandable_title text5"><?php echo cuhk_multilang_text("研究興趣","","Research Interests"); ?><div class="icon"></div>
-							</div>
-							<div class="hidden">
-								<div class="hidden_content">
-									<div class="free_text">
-										<?php echo wp_kses_post($research_interests); ?>
+							<div class="col10 center_content">
+								<div class="expandable_title text5"><?php echo cuhk_multilang_text("研究興趣","","Research Interests"); ?><div class="icon"></div>
+								</div>
+								<div class="hidden">
+									<div class="hidden_content">
+										<div class="free_text">
+											<?php echo wp_kses_post($research_interests); ?>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -198,12 +201,14 @@ if (have_posts()) :
 				<?php if ($teaching): ?>
 					<div class="expandable_item">
 						<div class="section_center_content small_section_center_content">
-							<div class="expandable_title text5"><?php echo cuhk_multilang_text("任教科目","","Teaching"); ?><div class="icon"></div>
-							</div>
-							<div class="hidden">
-								<div class="hidden_content">
-									<div class="free_text">
-										<?php echo wp_kses_post($teaching); ?>
+							<div class="col10 center_content">
+								<div class="expandable_title text5"><?php echo cuhk_multilang_text("任教科目","","Teaching"); ?><div class="icon"></div>
+								</div>
+								<div class="hidden">
+									<div class="hidden_content">
+										<div class="free_text">
+											<?php echo wp_kses_post($teaching); ?>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -214,12 +219,14 @@ if (have_posts()) :
 				<?php if ($selected_publications): ?>
 					<div class="expandable_item">
 						<div class="section_center_content small_section_center_content">
-							<div class="expandable_title text5"><?php echo cuhk_multilang_text("著作選錄","著作选录","Selected Publications"); ?><div class="icon"></div>
-							</div>
-							<div class="hidden">
-								<div class="hidden_content">
-									<div class="free_text">
-										<?php echo wp_kses_post($selected_publications); ?>
+							<div class="col10 center_content">
+								<div class="expandable_title text5"><?php echo cuhk_multilang_text("著作選錄","著作选录","Selected Publications"); ?><div class="icon"></div>
+								</div>
+								<div class="hidden">
+									<div class="hidden_content">
+										<div class="free_text">
+											<?php echo wp_kses_post($selected_publications); ?>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -230,29 +237,31 @@ if (have_posts()) :
 				<?php if (have_rows('research_projects')): ?>
 					<div class="expandable_item">
 						<div class="section_center_content small_section_center_content">
-							<div class="expandable_title text5"><?php echo cuhk_multilang_text("研究計劃","","Research Projects"); ?><div class="icon"></div>
-							</div>
-							<div class="hidden">
-								<div class="hidden_content">
-									<div class="people_detail_content_year_list">
-										<table>
-											<thead>
-												<tr>
-													<td class="year"><?php echo cuhk_multilang_text("年度","","Year"); ?></td>
-													<td class="free_text"><?php echo cuhk_multilang_text("研究","","Research"); ?></td>
-												</tr>
-											</thead>
-											<tbody>
-												<?php while (have_rows('research_projects')): the_row(); ?>
+							<div class="col10 center_content">
+								<div class="expandable_title text5"><?php echo cuhk_multilang_text("研究計劃","","Research Projects"); ?><div class="icon"></div>
+								</div>
+								<div class="hidden">
+									<div class="hidden_content">
+										<div class="people_detail_content_year_list">
+											<table>
+												<thead>
 													<tr>
-														<td class="year text5"><?php the_sub_field('year'); ?></td>
-														<td class="free_text">
-															<?php echo wp_kses_post(get_sub_field('free_text')); ?>
-														</td>
+														<td class="year"><?php echo cuhk_multilang_text("年度","","Year"); ?></td>
+														<td class="free_text"><?php echo cuhk_multilang_text("研究","","Research"); ?></td>
 													</tr>
-												<?php endwhile; ?>
-											</tbody>
-										</table>
+												</thead>
+												<tbody>
+													<?php while (have_rows('research_projects')): the_row(); ?>
+														<tr>
+															<td class="year text5"><?php the_sub_field('year'); ?></td>
+															<td class="free_text">
+																<?php echo wp_kses_post(get_sub_field('free_text')); ?>
+															</td>
+														</tr>
+													<?php endwhile; ?>
+												</tbody>
+											</table>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -263,12 +272,14 @@ if (have_posts()) :
 				<?php if ($other_positions_held): ?>
 					<div class="expandable_item">
 						<div class="section_center_content small_section_center_content">
-							<div class="expandable_title text5"><?php echo cuhk_multilang_text("其他職銜","","Other Positions Held"); ?><div class="icon"></div>
-							</div>
-							<div class="hidden">
-								<div class="hidden_content">
-									<div class="free_text">
-										<?php echo wp_kses_post($other_positions_held); ?>
+							<div class="col10 center_content">
+								<div class="expandable_title text5"><?php echo cuhk_multilang_text("其他職銜","","Other Positions Held"); ?><div class="icon"></div>
+								</div>
+								<div class="hidden">
+									<div class="hidden_content">
+										<div class="free_text">
+											<?php echo wp_kses_post($other_positions_held); ?>
+										</div>
 									</div>
 								</div>
 							</div>
@@ -279,29 +290,31 @@ if (have_posts()) :
 				<?php if (have_rows('awards_and_honors')): ?>
 					<div class="expandable_item">
 						<div class="section_center_content small_section_center_content">
-							<div class="expandable_title text5"><?php echo cuhk_multilang_text("獎項與榮譽","","Awards and Honors"); ?><div class="icon"></div>
-							</div>
-							<div class="hidden">
-								<div class="hidden_content">
-									<div class="people_detail_content_year_list">
-										<table>
-											<thead>
-												<tr>
-													<td class="year"><?php echo cuhk_multilang_text("年度","","Year"); ?></td>
-													<td class="free_text"><?php echo cuhk_multilang_text("獎項","","Awards"); ?></td>
-												</tr>
-											</thead>
-											<tbody>
-												<?php while (have_rows('awards_and_honors')): the_row(); ?>
+							<div class="col10 center_content">
+								<div class="expandable_title text5"><?php echo cuhk_multilang_text("獎項與榮譽","","Awards and Honors"); ?><div class="icon"></div>
+								</div>
+								<div class="hidden">
+									<div class="hidden_content">
+										<div class="people_detail_content_year_list">
+											<table>
+												<thead>
 													<tr>
-														<td class="year text5"><?php the_sub_field('year'); ?></td>
-														<td class="free_text">
-															<?php echo wp_kses_post(get_sub_field('free_text')); ?>
-														</td>
+														<td class="year"><?php echo cuhk_multilang_text("年度","","Year"); ?></td>
+														<td class="free_text"><?php echo cuhk_multilang_text("獎項","","Awards"); ?></td>
 													</tr>
-												<?php endwhile; ?>
-											</tbody>
-										</table>
+												</thead>
+												<tbody>
+													<?php while (have_rows('awards_and_honors')): the_row(); ?>
+														<tr>
+															<td class="year text5"><?php the_sub_field('year'); ?></td>
+															<td class="free_text">
+																<?php echo wp_kses_post(get_sub_field('free_text')); ?>
+															</td>
+														</tr>
+													<?php endwhile; ?>
+												</tbody>
+											</table>
+										</div>
 									</div>
 								</div>
 							</div>
