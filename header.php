5fa6fe508cc894e312f742a5b9d0ca1dd9e8886f
nekogato <05kcwong@gmail.com>
2025-09-03 21:29:40 +0800

    Update header.php

--- header.php
+++ header.php
@@ -18,6 +18,16 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-4JJ82FG4V2"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-4JJ82FG4V2');
+	</script>
 </head>
 <?php
 global $style_class;
