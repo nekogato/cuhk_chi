880c5b6d2261343877ec09b7092d84ada96e23b1
nekogato <05kcwong@gmail.com>
2025-09-04 12:01:19 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -11,27 +11,18 @@ get_header();
 // Include roll menu
 get_template_part('template-parts/roll-menu');
 
-if (have_posts()) :
-	while (have_posts()) : the_post();
 
 ?>
 
 	<div class="section section_content ">
 			<div class="section_center_content xs_section_center_content">
-				<?php if ($page_title): ?>
-					<h1 class="section_title text1 scrollin scrollinbottom"><?php echo cuhk_multilang_text("404找不到網頁。","","404 Page not found."); ?></h1>
-				<?php endif; ?>
+				<h1 class="section_title text1 scrollin scrollinbottom"><?php echo cuhk_multilang_text("404找不到網頁。","","404 Page not found."); ?></h1>
 
-				<?php if ($page_description): ?>
-					<div class="section_description scrollin scrollinbottom col6"><?php echo cuhk_multilang_text("很抱歉，我們未能提供你所找尋的路徑。該路徑也許不正確或已被移除。","","Sorry, the CUHK page you are looking for cannot be found. The URL address may be incorrect or the page may be outdated. "); ?></div>
-				<?php endif; ?>
-
+				<div class="section_description scrollin scrollinbottom col6"><?php echo cuhk_multilang_text("很抱歉，我們未能提供你所找尋的路徑。該路徑也許不正確或已被移除。","","Sorry, the CUHK page you are looking for cannot be found. The URL address may be incorrect or the page may be outdated. "); ?></div>
 			</div>
 		</div>
 
 
 <?php
-	endwhile;
-endif;
 
 get_footer(); ?>
