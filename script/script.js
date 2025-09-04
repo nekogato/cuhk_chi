2b09596a4a00e04fec39513ec2c40587dc3a16c4
nekogato <05kcwong@gmail.com>
2025-09-04 18:36:54 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -447,7 +447,7 @@ function init_event(){
 			$(".song_text .t2 .version_name").text(myname);
 			var mysong  = $this.attr("data-song");
 			changeAudioSource($(".audio-player"),mysong);
-			$(".download_song_wrapper a").attr("href",$this.attr(mysong));
+			$(".download_song_wrapper a").attr("href",mysong);
 		}
 		return false;
 	})
