273371a1101338a8d348d2d45e8be6ceba061b2e
nekogato <05kcwong@gmail.com>
2025-12-30 13:43:21 +0800

    Create tmp-newsletter.php

---
+++ tmp-newsletter.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Template Name: Newsletter
+ */
+
+get_header();
+
+// Include roll menu
+get_template_part('template-parts/roll-menu');
+
+if (have_posts()) :
+	while (have_posts()) : the_post();
+
+	
+?>
+
+		<div class="section section_content ">
+			<img src="<?php echo get_template_directory_uri(); ?>/images/ink_bg9.jpg" class="ink_bg9 scrollin scrollinbottom" alt="Background">
+			<div class="section_center_content xs_section_center_content">
+				<?php if ($page_title): ?>
+					<h1 class="section_title text1 scrollin scrollinbottom"><?php echo wp_kses_post($page_title); ?></h1>
+				<?php endif; ?>
+
+				<?php if ($page_description): ?>
+					<div class="section_description scrollin scrollinbottom col6"><?php echo wp_kses_post($description); ?></div>
+				<?php endif; ?>
+
+				<div class="section_introduction col12 scrollin scrollinbottom">
+					<div class="free_text">
+						<?php the_content(); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+<?php
+	endwhile;
+endif;
+
+get_footer(); ?>
